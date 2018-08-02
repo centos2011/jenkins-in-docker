4b587ca4784b201b19b1cc6a09417de2c5423c44
Ubuntu <ubuntu@ip-10-0-0-151.ap-southeast-1.compute.internal>
2018-08-02 08:57:20 +0000

    Updated container-run.sh Added auto-start on container

--- images/master/bin/container-run.sh
+++ images/master/bin/container-run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 docker run \
-    --restart=always \
     -d \
+    -i \
+    -t \
     --name jenkins-master \
     -p 8080:8080 \
     -p 50000:50000 \
+    --restart on-failure \
     -v $(which docker):/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /root/workspace \
